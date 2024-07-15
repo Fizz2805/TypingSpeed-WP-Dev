9f1b94d932965eed2b5fa9112009b8a41b5309eb
unknown <fizzajavaid765.com>
2024-07-15 13:22:53 -0700

    first commit

---
+++ page-login.php
@@ -0,0 +1,30 @@
+<!-- <php
+/* Template Name: Login */
+get_header();
+?>
+<div class="login-form">
+    <form action="<php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+        <input type="hidden" name="action" value="custom_login">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</div>
+<php
+get_footer();
+?> -->
+<?php
+/* Template Name: Login */
+get_header();
+?>
+<div class="login-form">
+    <form action="<?php echo esc_url(home_url('/test')); ?>" method="POST">
+        <input type="hidden" name="action" value="custom_login">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</div>
+<?php
+get_footer();
+?>
